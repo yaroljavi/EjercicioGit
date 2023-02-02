9d254d7b672cafb4c6122202b10b6945f0d9c354
dam <dam@203P16>
2023-02-02 09:59:19 +0100

    Líneas en el método main

---
+++ src/ejerciciogit/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author dam
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Primer sout del proyecto");
+        System.out.println("Segundo sout del proyecto");
+    }
+}
